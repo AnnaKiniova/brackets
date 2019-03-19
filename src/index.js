8b7518220b5a9ec55a309f8c6b569e4ad49d61fa
Anna Kiniova <anna.kiniova@gmail.com>
2019-03-19 23:40:35 +0300

    feat: - new solution add all items to arry result and then check

--- src/index.js
+++ src/index.js
@@ -1,27 +1,57 @@
 module.exports = function check(str, bracketsConfig) {
   
-  let lastOpen = "";
-  let firstClose = "";
+  // let lastOpen = "";
+  // let firstClose = "";
   
-
   let result = [];
-  for (let i=0; i<str.length; i++)
-    for (let j=0; j< bracketsConfig.length; j++) {
-      
-      if (str[i] == bracketsConfig[j][0]) {
-        result.push(str[i]);
-        // lastOpen = result[result.length-1];
-        
-         }
-      else  
-        if ((str[i] == bracketsConfig[j][1])&&(bracketsConfig[j][0] == result[result.length-1]))
-        result.pop();
-        
-        // else 
-        //   result.push(str[i]);
+  for (let i=0; i<str.length; i++) {
+    result.push(str[i]);
+  
+      for (let j=0; j< bracketsConfig.length; j++) {
         
 
+      // if ((str[i] == bracketsConfig[j][0])||(str[i] == bracketsConfig[j][1])) {}
+      
+
+        {if  ((str[i] == bracketsConfig[j][1])&&(result.length<2)&&(bracketsConfig[j][0] !== bracketsConfig[j][1]))
+          return false;
+        
+        else 
+          if ((str[i] == bracketsConfig[j][1])&&(bracketsConfig[j][0] == result[(result.length-2)]))
+          {result.pop();
+          result.pop();
+          break;}
+       }
+
+          // else results.pop();
+          
       }
+      
+  }
+      
+
+
+  // let result = [];
+  // for (let i=0; i<str.length; i++)
+  //   for (let j=0; j< bracketsConfig.length; j++) {
+  //       if ((str[i] == bracketsConfig[j][1])&&(bracketsConfig[j][0] == result[result.length-1])) {
+  //         result.pop();
+  //         break;
+  //       }
+
+          
+  //       else 
+  //         if (str[i] == bracketsConfig[j][0]) {
+  //           result.push(str[i]);
+  //           break;
+  //         }
+        
+  //         if ((str[i] == bracketsConfig[j][1])&&(result.length==0)) {
+  //           return false;
+  //         }
+        
+
+      
     
     
     if (result.length > 0) 
@@ -32,4 +62,4 @@ module.exports = function check(str, bracketsConfig) {
   
   // your solution
 }
-//  check('[(])', [['(', ')'], ['[', ']']]);
+//check('{[()]}', [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']]);
