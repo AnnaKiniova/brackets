ed8f1417d2c78dad6d14ca359850894416ddbe05
Anna Kiniova <anna.kiniova@gmail.com>
2019-03-19 21:58:51 +0300

    fix: - add for

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
+  
+  let lastOpen = "";
+  let firstClose = "";
+  
+
+  let result = [];
+  for (let i=0; i<str.length; i++)
+    for (let j=0; j< bracketsConfig.length; j++) {
+      
+      if (str[i] == bracketsConfig[j][0]) {
+        result.push(str[i]);
+        // lastOpen = result[result.length-1];
+        
+         }
+      else  
+        if ((str[i] == bracketsConfig[j][1])&&(bracketsConfig[j][0] == result[result.length-1]))
+        result.pop();
+        
+        // else 
+        //   result.push(str[i]);
+        
+
+      }
+    
+    
+    if (result.length > 0) 
+        return false;
+      else 
+        return true;
+  
+  
   // your solution
 }
+//  check('[(])', [['(', ')'], ['[', ']']]);
